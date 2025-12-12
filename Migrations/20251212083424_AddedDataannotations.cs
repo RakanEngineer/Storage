d6fe12b408548e5413bc8333cbe6ac461ca06720
RakanEngineer <rakan.engineer@gmail.com>
2025-12-12 09:37:45 +0100

    Added dataannotations

---
+++ Migrations/20251212083424_AddedDataannotations.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Storage.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedDataannotations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Shelf",
+                table: "Product",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Product",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Product",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Category",
+                table: "Product",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Shelf",
+                table: "Product",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(10)",
+                oldMaxLength: 10);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Product",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Product",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Category",
+                table: "Product",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(10)",
+                oldMaxLength: 10);
+        }
+    }
+}
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,14 +1,23 @@
-﻿namespace Storage.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Storage.Models
 {
     public class Product
     {
         public int Id { get; set; }
+        [StringLength(20, ErrorMessage = "Max 20")] 
         public string Name { get; set; }
+        [Range(1, 999)] 
         public decimal Price { get; set; }
+        [DataType(DataType.Date)] 
         public DateTime Orderdate { get; set; }
+        [StringLength(10, ErrorMessage = "Max 10")]        
         public string Category { get; set; }
+        [StringLength(10, ErrorMessage = "Max 10")]
         public string Shelf { get; set; }
+        [Range(0, 10000)]
         public int Count { get; set; }
+        [StringLength(500, ErrorMessage = "Max 500")]
         public string Description { get; set; }
 
     }
